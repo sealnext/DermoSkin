12e2fb86ca52a92d34d539c9acc81613ef543992
Andrei Badescu <andreibadescu18@yahoo.com>
2023-08-12 02:42:58 +0300

    Dropped some platforms
    Removed following platforms: linux, macos, web, windows.

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:dermo/ui/pages/home_page.dart';
-import 'package:dermo/logic/services/Auth.dart';
+import 'package:dermo/logic/services/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
