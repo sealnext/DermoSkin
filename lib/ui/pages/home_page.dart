55a352b256f7833dba50768411d60c2113c7e45c
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-08-12 14:06:01 +0300

    Added home page
    Delete .vscode directory
    
    Delete linux/flutter directory

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:dermo/ui/pages/auth_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:dermo/utils/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +12,20 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
+  int _currentIndex = 0;
+
+  Widget _buildIcon(IconData iconData, int index) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: _currentIndex == index ? AppColor.primary : Colors.transparent,
+      ),
+      child:
+          Icon(iconData, color: _currentIndex == index ? Colors.white : null),
+    );
+  }
 
   void signOut() {
     FirebaseAuth.instance.signOut();
@@ -20,32 +35,346 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _buildAppointment({
+    required String doctorName,
+    required String doctorProfession,
+    required String reason,
+    required String date,
+    required String duration,
+    required String clinicLocation,
+    required String imageUrl,
+  }) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                backgroundColor: AppColor.gray,
+                backgroundImage: NetworkImage(imageUrl),
+                radius: 25, // ajustează dimensiunea dacă este necesar
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("DR. $doctorName"),
+                    Text(doctorProfession),
+                    Text(
+                      reason,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildAppointmentDetail(Icons.calendar_today, date),
+              _buildAppointmentDetail(Icons.access_time, duration),
+              _buildAppointmentDetail(Icons.local_hospital, clinicLocation),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              Expanded(
+                flex: 6,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          10.0), // Acesta setează un borderRadius de 10.0
+                    ),
+                    // alte stiluri pe care doriți să le setați
+                  ),
+                  child: const Text("See details"),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          10.0), // Acesta setează un borderRadius de 10.0
+                    ),
+                    // alte stiluri pe care doriți să le setați
+                  ),
+                  child: const AspectRatio(
+                    aspectRatio: 1, // acesta este raportul 1:1
+                    child: AspectRatio(
+                      aspectRatio: 1, // acesta este raportul 1:1
+                      child: Center(
+                        child: Text(
+                          '...',
+                          style: TextStyle(
+                            color: Colors
+                                .black, // Acesta setează culoarea textului la negru
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppointmentDetail(IconData icon, String info) {
+    return Column(
+      children: [
+        Icon(icon),
+        const SizedBox(height: 4),
+        Text(info),
+      ],
+    );
+  }
+
+  Widget _buildSmallItem({required IconData icon, required String label}) {
+    return Expanded(
+      child: Padding(
+        padding:
+            const EdgeInsets.symmetric(vertical: 16.0), // Spatiu sus si jos
+        child: AspectRatio(
+          aspectRatio: 1, // Acest lucru asigură că widget-ul rămâne un pătrat
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, color: AppColor.primary, size: 30),
+                const SizedBox(height: 8),
+                Text(
+                  label,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          if (FirebaseAuth.instance.currentUser != null)
-            IconButton(
-              onPressed: signOut,
-              icon: const Icon(Icons.logout),
-            )
-        ],
-        backgroundColor: const Color(0xFF4757E9),
-        title: const Text(
-          'DermoSkin',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
+      backgroundColor: AppColor.backgroundPrimary,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hello ${user.email ?? 'User'}",
+                            style: const TextStyle(fontSize: 24)),
+                        const Text(
+                          "Keep healthy!",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    radius: 30,
+                    // Replace with actual image
+                    backgroundImage: NetworkImage(
+                        'https://i.pinimg.com/564x/74/d7/b0/74d7b05c3476e062ca7c26452ffb22cb.jpg'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    const Icon(Icons.search),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          hintText: 'Search a doctor',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.fromLTRB(15, 30, 15, 30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColor.primary,
+                ),
+                child: const Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Aliniați conținutul la stânga
+                  children: [
+                    Text(
+                      "Tips for fighting \nacne",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                    SizedBox(
+                        height:
+                            10), // Adăugați spațiu între text și rândul cu icon
+                    Row(
+                      children: [
+                        Icon(Icons.visibility, color: Colors.white),
+                        SizedBox(width: 5),
+                        Text("x views", style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  _buildSmallItem(icon: Icons.person, label: 'Doctor'),
+                  const SizedBox(width: 16.0), // Ajustează lățimea după nevoi
+                  _buildSmallItem(
+                      icon: Icons.local_hospital, label: 'Hospital'),
+                  const SizedBox(width: 16.0), // Ajustează lățimea după nevoi
+                  _buildSmallItem(icon: Icons.headset_mic, label: 'Consultant'),
+                  const SizedBox(width: 16.0), // Ajustează lățimea după nevoi
+                  _buildSmallItem(icon: Icons.book_online, label: 'Recipe'),
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Your appointments",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    "See all",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    _buildAppointment(
+                      doctorName: "John Doe",
+                      doctorProfession: "Cardiologist",
+                      reason: "Monthly check-up",
+                      date: "15/05/23",
+                      duration: "09:15-10:10",
+                      clinicLocation: "RS. Sardjito",
+                      imageUrl:
+                          "https://www.jeanlouismedical.com/img/doctor-profile-small.png",
+                    ),
+                    _buildAppointment(
+                      doctorName: "John Doe",
+                      doctorProfession: "Cardiologist",
+                      reason: "Monthly check-up",
+                      date: "15/05/23",
+                      duration: "09:15-10:10",
+                      clinicLocation: "RS. Sardjito",
+                      imageUrl:
+                          "https://www.henrymayo.com/app/files/public/dhanda.l--0002.jpg",
+                    ),
+                    // Adaugă mai multe programări aici
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-        automaticallyImplyLeading:
-            false, // Prevent the back button from being shown
       ),
-      body: SafeArea(
-        child: Center(
-          child: Text("Sunt logat yeyeyey XD${user.email!}"),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColor.white,
+        elevation: 0.0,
+        showSelectedLabels:
+            true, // sau `false` dacă doriți să eliminați etichetele
+        showUnselectedLabels:
+            true, // sau `false` dacă doriți să eliminați etichetele
+        selectedItemColor: AppColor.primary, // Culoarea elementului selectat
+        unselectedItemColor: Colors.grey, // Culoarea elementelor neselectate
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.home, 0),
+            label: '',
+            activeIcon: _buildIcon(Icons.home, 0), // Se adaugă aceasta linie
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.calendar_today, 1),
+            label: '',
+            activeIcon: _buildIcon(Icons.calendar_today, 1),
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.chat, 2),
+            label: '',
+            activeIcon: _buildIcon(Icons.chat, 2),
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.settings, 3),
+            label: '',
+            activeIcon: _buildIcon(Icons.settings, 3),
+          ),
+        ],
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 @immutable
 class AppColor {
-  final white = Colors.white;
-  final primary = const Color(0xFF4757E9);
-  final secondary = const Color(0xFF20A6BE);
-  final ternary = const Color(0xFFACDECE);
-  final backgroundPrimary = const Color.fromARGB(255, 217, 217, 217);
-  final backgroundSecondary = Colors.white;
-  final accent1 = const Color.fromARGB(255, 255, 242, 0);
-  final accent2 = const Color.fromARGB(255, 255, 67, 67);
+  static const white = Colors.white;
+  static const primary = Color(0xFF2B40FF);
+  static const secondary = Color.fromARGB(255, 170, 179, 255);
+  static const ternary = Color(0xFFACDECE);
+  static const backgroundPrimary = Color(0xFFF6F9F9);
+  static const backgroundSecondary = Colors.white;
+  static const accent1 = Color.fromARGB(255, 255, 242, 0);
+  static const accent2 = Color.fromARGB(255, 255, 67, 67);
+  static const gray = Color.fromARGB(255, 233, 233, 233);
 }
