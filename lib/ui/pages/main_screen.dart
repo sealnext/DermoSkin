aaab5642bef2dd324036057c71b95ed5ae515806
Ovidiu Bachmatchi <ovidiubachmatchi@gmail.com>
2023-09-04 13:23:10 +0300

    nav bar

--- lib/ui/pages/main_screen.dart
+++ lib/ui/pages/main_screen.dart
@@ -1,10 +1,33 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dermo/core/resources/color_manager.dart';
+import 'package:dermo/old/ui/pages/appointments/appointments.dart';
 import 'package:dermo/ui/routes/app_router.gr.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+// 1. Wrap Icon in Center Widget
+  Widget _buildIcon(IconData iconData, int index) {
+    return Center(
+      child: SizedBox(
+        height: 40,
+        child: Center(
+          child: Icon(
+            iconData,
+            color: _currentIndex == index ? ThemeColors.primary : Colors.grey,
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,26 +38,40 @@ class MainScreen extends StatelessWidget {
         SettingsRoute(),
       ],
       bottomNavigationBuilder: (_, tabsRouter) {
-        return NavigationBar(
-          selectedIndex: tabsRouter.activeIndex,
-          onDestinationSelected: tabsRouter.setActiveIndex,
-          destinations: const [
-            NavigationDestination(
-              selectedIcon: Icon(Icons.home),
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(Icons.chat),
-              icon: Icon(Icons.chat_outlined),
-              label: 'Chat',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(Icons.settings),
-              icon: Icon(Icons.settings_outlined),
-              label: 'Settings',
-            ),
-          ],
+        return SizedBox(
+          height: 125, // Setează înălțimea dorită
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+              tabsRouter.setActiveIndex(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: _buildIcon(Icons.home, 0),
+                label: '',
+                activeIcon: _buildIcon(Icons.home, 0),
+              ),
+              BottomNavigationBarItem(
+                icon: _buildIcon(Icons.chat, 1),
+                label: '',
+                activeIcon: _buildIcon(Icons.chat, 1),
+              ),
+              BottomNavigationBarItem(
+                icon: _buildIcon(Icons.settings, 2),
+                label: '',
+                activeIcon: _buildIcon(Icons.settings, 2),
+              ),
+            ],
+          ),
         );
       },
     );
